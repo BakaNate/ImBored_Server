5205d14031bff2fcaa209bf324a04a04b494b13c
Nathan <nathan.bonnet@epitech.eu>
2019-11-06 14:26:42 +0100

    CRLF fixed

--- src/tools/validations/userValidate.js
+++ src/tools/validations/userValidate.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi');
 const userValidate = async function (data) {
   const schema = Joi.object({
     userEmail: Joi.string()
-      .pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+      .pattern(/^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
       .required(),
 
     userPswd: Joi.string()
